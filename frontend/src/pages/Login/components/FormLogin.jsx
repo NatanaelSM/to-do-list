8e57e712bca466f778c059d1dece808ee5f080b1
Natanael <natanaelsm0102@gmail.com>
2024-08-02 11:07:48 -0300

    feat: Página login feita

--- frontend/src/pages/Login/components/FormLogin.jsx
+++ frontend/src/pages/Login/components/FormLogin.jsx
@@ -1,21 +1,26 @@
 import '../styles/formStyle.css'
 
 export const FormLogin = () => {
-    return(
-        <>
-            <form>
+    return (
+        <div className='container-form'>
+            <form className='form'>
+                <h1 className='header-form'>LOGIN</h1>
                 <div className="form-group">
                     <label for='usuario'>Usuário</label>
-                    <input type="text" id='usuario' name='usuario' required/>
+                    <input type="text" id='usuario' name='usuario' required />
                 </div>
                 <div className="form-group">
                     <label for='senha'>Senha</label>
-                    <input type="password" id='senha' name='senha' required/>
+                    <input type="password" id='senha' name='senha' required />
                 </div>
                 <div className="form-group">
                     <button type="submit">Entrar</button>
                 </div>
             </form>
-        </>
+            <div className='cadastro-form'>
+                <span>Não possui cadastro?</span>
+                <a href='#'><u>cadastre-se</u></a>
+            </div>
+        </div>
     )
 }
